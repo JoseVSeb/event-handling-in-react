7b2faf897a9acd6e21f7bd66caebe50a42bc8bed
Jose V Sebastian <josevseb99@gmail.com>
2021-10-13 00:09:56 +0530

    complete challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
   return (
     <div className="container">
       <h1>Hello</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+      >
+        Submit
+      </button>
     </div>
   );
 }
